d814ab447042740929b3cb52bf8ac370ae065424
JUAN RAFAEL MEJIA LAGUNA <juan.mejia@di.unanleon.edu.ni>
2021-07-16 08:45:23 -0600

    fix name var

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", function (req, res) {
     res.render('index');
 });
 
-const listener = app.listen(process.env.port || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
     console.log("sever started on port " + listener.address().port)
 });
